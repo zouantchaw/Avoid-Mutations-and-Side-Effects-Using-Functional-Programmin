191d97710947f6978e163da01c2d22a23587fd2a
Wielfried Zouantcha <44246692+zouantchaw@users.noreply.github.com>
2021-11-05 18:50:10 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Fill in the code for the function incrementer so it returns the value of the global variable fixedValue increased by one.
+
+
+// The global variable
+let fixedValue = 4;
+
+function incrementer() {
+  return fixedValue + 1
+}
+
+incrementer()
